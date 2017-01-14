d95e429c76a91be242085bba2a8d470ca81b119d
agamemnus <agamemnus@users.noreply.github.com>
2017-01-14 16:16:44 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,16 @@
-function make_granolas (line, n, a) {
+function make_granolas (complete, n, a) {
  console.log ("Producing granola " + (n + 1))
  setTimeout (function () {
   console.log ("Granola " + (n + 1) + " produced.")
-  line.complete ()
+ .complete ()
  }, parseInt(Math.random() * 20000 + 5000))
 }
 
-function make_fruit_tarts (line, n, a) {
+function make_fruit_tarts (complete, n, a) {
  console.log ("Producing fruit tart " + (n + 1))
  setTimeout (function () {
   console.log ("Fruit tart " + (n + 1) + " produced.")
-  line.complete ()
+  complete ()
  }, parseInt(Math.random() * 10000 + 2500))
 }
 
