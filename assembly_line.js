a8e659d78af16511a5d521dbeee8776565e94208
agamemnus <agamemnus@users.noreply.github.com>
2017-01-14 16:14:37 -0500

    Update assembly_line.js

--- assembly_line.js
+++ assembly_line.js
@@ -48,15 +48,3 @@ function make_fruit_tarts (line, n, a) {
   line.complete ()
  }, parseInt(Math.random() * 10000 + 2500))
 }
-
-//////////
-
-// queue.add (function_name, function, params)
-// queue.complete ()
-      
-var pastry_line = new AssemblyLine ({max_active: 5})
-
-for (var i = 0; i < 20; i++) {
- pastry_line.add ("granolas"   , make_granolas   , i, 5)
- pastry_line.add ("fruit_tarts", make_fruit_tarts, i, 6)
-}
