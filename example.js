7eb895cebfe3f9407bbb8a0d3658c801ce45f064
agamemnus <agamemnus@users.noreply.github.com>
2017-01-14 20:40:32 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 function make_granolas (line, n, a) {
  console.log ("Granola chef " + (line.index + 1) + " is producing granola " + (n + 1) + ".")
  setTimeout (function () {
-  console.log ("Granola " + (n + 1) + " produced.")
+ console.log ("Granola tart chef " + (line.index + 1) + " has produced granola " + (n + 1) + ".")
   line.complete ()
  }, Math.random() * 20000 + 5000)
 }
@@ -9,7 +9,7 @@ function make_granolas (line, n, a) {
 function make_fruit_tarts (line, n, a) {
  console.log ("Fruit tart chef " + (line.index + 1) + " is producing fruit tart " + (n + 1) + ".")
  setTimeout (function () {
-  console.log ("Fruit tart " + (n + 1) + " produced.")
+ console.log ("Fruit tart chef " + (line.index + 1) + " has produced fruit tart " + (n + 1) + ".")
   line.complete ()
  }, Math.random() * 10000 + 2500)
 }
@@ -19,7 +19,7 @@ function make_fruit_tarts (line, n, a) {
 // queue.add (function_name, function, params)
 // queue.complete ()
       
-var pastry_line = new AssemblyLine ({max_active: 5})
+var pastry_line = new Semaphore ({max_active_default: 5, max_active: {granolas: 10, fruit_tarts: 5}})
 
 for (var i = 0; i < 20; i++) {
  pastry_line.add ("granolas"   , make_granolas   , i, 5)
